847030ecdf56484472745a5cbd7040605b09f1f5
Gede Prasidha <gede.prasidha@gmail.com>
2022-06-02 16:46:28 +0700

    Reader.py update
    can read csv but haven't build objects yet

---
+++ src/Reader.py
@@ -0,0 +1,61 @@
+from msilib.schema import Error
+from traceback import print_tb
+
+from numpy import integer
+from Tasks import Task
+from Models import Model
+
+import os
+import pandas as pd
+
+
+def createListOfPredecessors(predecessors):
+    # If "predecessors" is a single integer, return a list of integer of 1 length
+    # If "predecessors" is a string containing integers separated by commas, return a list of integers
+    if (type(predecessors) == integer):
+        return [predecessors]
+    else:
+        temp_list = predecessors.split(",")
+        map_integers = map(int, temp_list)
+        return list(map_integers)
+
+# File must be located in the "testcase" directory and have the "txt" extension
+
+
+def ReadFile(filename):
+    try:
+        path_to_folder = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), '..', 'testcase'))
+        path_to_file = os.path.abspath(os.path.join(path_to_folder, filename))
+        df = pd.read_csv(path_to_file)
+        for i in range(0, df.__len__()):
+            current_row = df.iloc[i]
+            task_name = current_row['task']
+            num_of_models = current_row['num_models']
+            print(task_name)
+            print(num_of_models)
+            for i in range(0, num_of_models):
+                current_count = i + 1
+                model_name = current_row['model' +
+                                         str(current_count) + "_name"]
+                human_cost = current_row['model' + str(current_count) + "_h"]
+                machine_cost = current_row['model' + str(current_count) + "_r"]
+                combo_cost = current_row['model' + str(current_count) + "_hrc"]
+                # new_model = Model(task_name, model_name, human_cost, machine_cost, combo_cost)
+                print(model_name)
+                print(human_cost)
+                print(machine_cost)
+                print(combo_cost)
+            predecessor_list = createListOfPredecessors(
+                current_row['predecessor'])
+            initial_solution = current_row['initial_solution']
+            originStation = current_row['belongsToStation']
+            print(predecessor_list)
+            print(initial_solution)
+            print(originStation)
+            print()
+    except Error as err:
+        print(err)
+
+
+ReadFile("testcase1.csv")
--- src/Tasks.py
+++ src/Tasks.py
@@ -2,18 +2,20 @@ from Models import Model
 
 
 class Task:
+    # Constructor function
     def __init__(self, task_name, max_models):
         self.task_name = task_name
         self.max_models = max_models
         self.models = []
-        # self.predecessors = []
-        self.successors = []
+        self.predecessors = []
 
+    # Add a model to the task
     def addModel(self, model):
         if (self.models.__len__() < self.max_models):
             self.models.append(model)
         else:
             print("Error: Task " + self.task_name + " is full.")
 
+    # Add successor tasks to Task
     def addSuccessors(self, successor):
         self.successors.append(successor)
